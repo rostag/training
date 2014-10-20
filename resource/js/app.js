e39f62a3faa602da33b836997ce487934c17dda7
vreznik <vadim.reznik@gmail.com>
2014-10-21 01:13:11 +0300

    add normalized index html

---
+++ resource/js/app.js
@@ -0,0 +1,22 @@
+﻿(function(){
+
+	'use strict';
+	
+	angular
+		.module('js-lessons', ['ngRoute', 'controller.main'])
+		.config(routeCfg);
+		
+	routeCfg.$inject = ['$routeProvider'];
+	
+	function routeCfg($routeProvider){
+		$routeProvider
+			.when('/:folder/:file', {
+				templateUrl: function(template){
+					return 'view/'+template.folder+'/'+template.file+'.html';
+				}
+			}).otherwise({
+				redirectTo: '/'
+			});                                       
+	}
+	
+})();
---
+++ resource/js/controller/main.js
@@ -0,0 +1,22 @@
+﻿(function(){
+	'use strict';
+
+	angular
+		.module('controller.main', [])
+		.controller('mainCtrl', mainCtrl)
+
+	mainCtrl.$inject = ['$scope', '$http'];
+	
+	function mainCtrl($scope, $http){
+		var vm = this;
+		
+		$http.get('base/base.json')
+			.success(function(response){
+				vm.base = response;
+			})
+			.error(function(error){
+				console.log(error)
+			});
+	}
+
+})();
