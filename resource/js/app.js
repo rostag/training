4426bf998dc6ed2718ee993b2558ceaf69c0afc4
vreznik <vadim.reznik@gmail.com>
2014-10-21 01:15:29 +0300

    update index html

--- resource/js/app.js
+++ resource/js/app.js
@@ -3,20 +3,8 @@
 	'use strict';
 	
 	angular
-		.module('js-lessons', ['ngRoute', 'controller.main'])
-		.config(routeCfg);
+		.module('trainingApp', ['']);
 		
 	routeCfg.$inject = ['$routeProvider'];
-	
-	function routeCfg($routeProvider){
-		$routeProvider
-			.when('/:folder/:file', {
-				templateUrl: function(template){
-					return 'view/'+template.folder+'/'+template.file+'.html';
-				}
-			}).otherwise({
-				redirectTo: '/'
-			});                                       
-	}
 	
 })();
