56c1dd98297189430406a0efbbabbb06fc0e1edd
vreznik <vadim.reznik@gmail.com>
2014-12-02 00:29:49 +0200

    test node

--- resource/js/app.js
+++ resource/js/app.js
@@ -3,6 +3,39 @@
 	'use strict';
 	
 	angular
-		.module('trainingApp', []);
+		.module('trainingApp', [])
+		.controller('loginCtrl', loginCtrl);
+		
+		
+	loginCtrl.$inject = ['$scope', '$http'];
+	
+	function loginCtrl($scope, $http){
+		
+		var local = $scope;
+		
+		local.submitted = false;
+		
+		local.creds = {
+			email: '',
+			password: ''
+		};
+		
+		$scope.login = login;
+		
+		function login(){
+			if(local.loginForm.$valid){
+				$http({
+					method: 'post',
+					url: 'http://localhost:41424/api/login',
+					data: creds
+				}).success(function(res){
+					console.log(res);
+				});
+			} else{
+				local.submitted = true;
+			}
+		}
+		
+	}
 	
 })();
