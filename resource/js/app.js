2fdd68110366f27a306eaf31dc52980cdedd7a8a
vreznik <vadim.reznik@gmail.com>
2015-02-05 00:05:31 +0200

    update training app

--- resource/js/app.js
+++ resource/js/app.js
@@ -7,11 +7,17 @@
 		.controller('loginCtrl', loginCtrl);
 		
 		
-	loginCtrl.$inject = ['$scope', '$http'];
+	loginCtrl.$inject = ['$scope', '$http', '$q', '$log'];
 	
-	function loginCtrl($scope, $http){
+	function loginCtrl($scope, $http, $q, $log){
 		
 		var local = $scope;
+		var deferred = $q.defer();
+		var base = 'https://gltraining.herokuapp.com/api/';
+		
+		local.action = null;
+		
+		$log.info('Hello user!');
 		
 		local.submitted = false;
 		
@@ -20,25 +26,46 @@
 			password: ''
 		};
 		
+		wakeUp().then(function(data){
+			$log.info('Server is ' + (data.started ? '' : 'not ') + 'started');
+		}, function(error){
+			$log.error(error);
+		});
+		
 		$scope.login = login;
 		
+		function wakeUp(){
+			$log.info('Starting application...');
+			$http.get(base+'wakeup', {timeout: deferred})
+				.success(function(res){
+					$log.info(res);
+					deferred.resolve({started: true});
+				})
+				.error(function(e){
+					$log.error(e);
+					deferred.reject(e);
+				});
+			return deferred.promise;
+		}
+		
 		function login(){
+			local.creds.password = hex_hmac_md5(local.creds.password, "glwebtraining");
+			
 			if(local.loginForm.$valid){
 				$http({
 					method: 'post',
-					url: 'https://gltraining.herokuapp.com/api/login',
+					url: base + 'log' + local.action,
 					data: local.creds,
 					headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'}
 				}).success(function(res){
-					console.log(res);
+					$log.info(res);
 				}).error(function(data){
-					console.log('Error:', data);
+					$log.error(data);
 				});
 			} else{
 				local.submitted = true;
 			}
-		}
-		
+		}		
 	}
 	
 })();
